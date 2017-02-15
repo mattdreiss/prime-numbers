2d23d49bb0104bf57feeff3c92aae200db3c4da0
mattdreiss <matt.dreiss@gmail.com>
2017-02-15 09:16:09 -0500

    Adding implementation for to determine if a number is prime and generate the range

--- src/main/java/prime/PrimeNumberGeneratorImpl.java
+++ src/main/java/prime/PrimeNumberGeneratorImpl.java
@@ -1,5 +1,6 @@
 package prime;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,12 +10,48 @@ public class PrimeNumberGeneratorImpl implements PrimeNumberGenerator {
 
     @Override
     public List<Integer> generate(int startingValue, int endingValue) {
-        return null;
+        int pos = startingValue;
+        int rangeEnd = endingValue;
+        List<Integer> primes = new ArrayList<>();
+
+        if (startingValue > endingValue) {
+            pos = endingValue;
+            rangeEnd = startingValue;
+        }
+
+        while (pos <= rangeEnd) {
+            if (isPrime(pos)) {
+                primes.add(pos);
+            }
+            pos++;
+        }
+
+        return primes;
     }
 
     @Override
     public boolean isPrime(int value) {
-        return false;
+        if (value < 2) {
+            return false;
+        }
+
+        if (value < 4) {
+            return true;
+        }
+
+        if (value % 2 == 0 || value % 3 == 0) {
+            return false;
+        }
+
+        int maxDivisor = (int) Math.sqrt(value) + 1;
+
+        for (int i = 5; i < maxDivisor; i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
