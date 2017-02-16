5cd3e92439dbd3c363d512574c4b54edb4004233
mattdreiss <matt.dreiss@gmail.com>
2017-02-15 21:03:56 -0500

    This test case now handles both generators

--- src/test/java/prime/DivisionPrimeNumberGeneratorTest.java
+++ src/test/java/prime/DivisionPrimeNumberGeneratorTest.java
@@ -14,72 +14,93 @@ import static org.junit.Assert.*;
  */
 public class DivisionPrimeNumberGeneratorTest {
 
-    private PrimeNumberGenerator generator;
+    private PrimeNumberGenerator generatorA;
+
+    private PrimeNumberGenerator generatorB;
 
     @Before
     public void setup() {
-        generator = new DivisionPrimeNumberGenerator();
+        generatorA = new DivisionPrimeNumberGenerator();
+        generatorB = new SieveOfEratosthenesGenerator();
     }
 
     @Test
     public void isPrime_negative1() throws Exception {
-        assertFalse(generator.isPrime(-1));
+        assertFalse(generatorA.isPrime(-1));
+        assertFalse(generatorB.isPrime(-1));
     }
 
     @Test
     public void isPrime_0() throws Exception {
-        assertFalse(generator.isPrime(0));
+        assertFalse(generatorA.isPrime(0));
+        assertFalse(generatorB.isPrime(0));
     }
 
     @Test
     public void isPrime_1() throws Exception {
-        assertFalse(generator.isPrime(1));
+        assertFalse(generatorA.isPrime(1));
+        assertFalse(generatorB.isPrime(1));
     }
 
     @Test
     public void isPrime_2() throws Exception {
-        assertTrue(generator.isPrime(2));
+        assertTrue(generatorA.isPrime(2));
+        assertTrue(generatorB.isPrime(2));
     }
 
     @Test
     public void isPrime_3() throws Exception {
-        assertTrue(generator.isPrime(3));
+        assertTrue(generatorA.isPrime(3));
+        assertTrue(generatorB.isPrime(3));
     }
 
     @Test
     public void isPrime_4() throws Exception {
-        assertFalse(generator.isPrime(4));
+        assertFalse(generatorA.isPrime(4));
+        assertFalse(generatorB.isPrime(4));
     }
 
     @Test
     public void isPrime_5() throws Exception {
-        assertTrue(generator.isPrime(5));
+        assertTrue(generatorA.isPrime(5));
+        assertTrue(generatorB.isPrime(5));
     }
 
     @Test
     public void isPrime_9() throws Exception {
-        assertFalse(generator.isPrime(9));
+        assertFalse(generatorA.isPrime(9));
+        assertFalse(generatorB.isPrime(9));
     }
 
     @Test
     public void isPrime_7901() throws Exception {
-        assertTrue(generator.isPrime(7901));
+        assertTrue(generatorA.isPrime(7901));
+        assertTrue(generatorB.isPrime(7901));
     }
 
     @Test
     public void isPrime_7907() throws Exception {
-        assertTrue(generator.isPrime(7907));
+        assertTrue(generatorA.isPrime(7907));
+        assertTrue(generatorB.isPrime(7907));
     }
 
     @Test
     public void isPrime_7919() throws Exception {
-        assertTrue(generator.isPrime(7919));
+        assertTrue(generatorA.isPrime(7919));
+        assertTrue(generatorB.isPrime(7919));
     }
 
     @Test
     public void testRange_1to10() {
         List<Integer> expected = Arrays.asList(2, 3, 5, 7);
-        List<Integer> actual = generator.generate(1, 10);
+        List<Integer> actual = generatorA.generate(1, 10);
+
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(Ordering.natural().isOrdered(actual));
+
+        actual = generatorB.generate(1, 10);
 
         assertNotNull(actual);
         assertEquals(expected.size(), actual.size());
@@ -90,7 +111,14 @@ public class DivisionPrimeNumberGeneratorTest {
     @Test
     public void testRange_1to10Reverse() {
         List<Integer> expected = Arrays.asList(2, 3, 5, 7);
-        List<Integer> actual = generator.generate(10, 1);
+        List<Integer> actual = generatorA.generate(10, 1);
+
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(Ordering.natural().isOrdered(actual));
+
+        actual = generatorB.generate(10, 1);
 
         assertNotNull(actual);
         assertEquals(expected.size(), actual.size());
@@ -101,7 +129,14 @@ public class DivisionPrimeNumberGeneratorTest {
     @Test
     public void testRange_1to1000() {
         List<Integer> expected = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997);
-        List<Integer> actual = generator.generate(1, 1000);
+        List<Integer> actual = generatorA.generate(1, 1000);
+
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(Ordering.natural().isOrdered(actual));
+
+        actual = generatorB.generate(1, 1000);
 
         assertNotNull(actual);
         assertEquals(expected.size(), actual.size());
@@ -112,7 +147,14 @@ public class DivisionPrimeNumberGeneratorTest {
     @Test
     public void testRange_1to1000Reverse() {
         List<Integer> expected = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997);
-        List<Integer> actual = generator.generate(1000, 1);
+        List<Integer> actual = generatorA.generate(1000, 1);
+
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(Ordering.natural().isOrdered(actual));
+
+        actual = generatorB.generate(1000, 1);
 
         assertNotNull(actual);
         assertEquals(expected.size(), actual.size());
@@ -123,7 +165,14 @@ public class DivisionPrimeNumberGeneratorTest {
     @Test
     public void testRange_7900to7920() {
         List<Integer> expected = Arrays.asList(7901, 7907, 7919);
-        List<Integer> actual = generator.generate(7900, 7920);
+        List<Integer> actual = generatorA.generate(7900, 7920);
+
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(Ordering.natural().isOrdered(actual));
+
+        actual = generatorB.generate(7900, 7920);
 
         assertNotNull(actual);
         assertEquals(expected.size(), actual.size());
@@ -134,7 +183,14 @@ public class DivisionPrimeNumberGeneratorTest {
     @Test
     public void testRange_7900to7920Reverse() {
         List<Integer> expected = Arrays.asList(7901, 7907, 7919);
-        List<Integer> actual = generator.generate(7920, 7900);
+        List<Integer> actual = generatorA.generate(7920, 7900);
+
+        assertNotNull(actual);
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+        assertTrue(Ordering.natural().isOrdered(actual));
+
+        actual = generatorB.generate(7920, 7900);
 
         assertNotNull(actual);
         assertEquals(expected.size(), actual.size());
