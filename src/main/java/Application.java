eec15d6668698fb3b1fe3cec51fc7fa243f9b98d
mattdreiss <matt.dreiss@gmail.com>
2017-02-16 14:24:14 -0500

    added a command line main application to accept input via the command line

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,38 @@
-package PACKAGE_NAME;
+import prime.PrimeNumberGenerator;
+import prime.SieveOfEratosthenesGenerator;
+
+import java.util.List;
 
 /**
  * Created by mrd on 2/16/17.
  */
 public class Application {
+
+    public static void main(String[] args) {
+        int startingValue, endingValue;
+        try {
+            startingValue = Integer.parseInt(args[0]);
+            endingValue = Integer.parseInt(args[1]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            displayHelp();
+            return;
+        }
+
+        PrimeNumberGenerator generator = new SieveOfEratosthenesGenerator();
+        List<Integer> primes = generator.generate(startingValue, endingValue);
+
+        for (int i = 0; i < primes.size(); i++) {
+            if (i == primes.size() - 1) {
+                System.out.println(primes.get(i));
+            } else {
+                System.out.print(String.format("%s, ", primes.get(i)));
+            }
+        }
+
+    }
+
+    private static void displayHelp() {
+        System.out.println("Usage: prime-numbers <range-start> <range-end> where <range-start> and <range-end> are integers");
+    }
+
 }
